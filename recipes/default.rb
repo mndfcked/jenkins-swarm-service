9b361f9c54c497a22b0308dd5104c1452c2a78c1
Jörn D <j.domnik@googlemail.com>
2014-09-25 01:32:25 +0200

    Fixed stuff such that it kind of works now

--- recipes/default.rb
+++ recipes/default.rb
@@ -25,13 +25,13 @@
 service_is_installed = is_installed?()
 
 setup_zip = ::File.basename("jenkins-swarm-service.zip")
-setup_zip_basename = ::File.basename(setup_zip, ".zip");
+setup_zip_basename = ::File.basename(setup_zip, ".zip")
 setup_zip_temp_path = win_friendly_path(File.join(Dir.tmpdir(), setup_zip_basename))
-service_install_path = win_friendly_path(File.join(node['jekinsswarmservice']['install_path'], setup_zip_basename))
-service_config_file = win_friendly_path(File.join("config", "wrapper.confg"))
-swarm_client_path = win_friendly_path(File.join(service_install_path, "swarm-client.jar")
+service_install_path = node['jenkinsswarmservice']['install_path']
+service_config_file = win_friendly_path(File.join(service_install_path, "conf", "wrapper.conf"))
+swarm_client_path = win_friendly_path(File.join(service_install_path, "swarm-client.jar"))
 setup_log_path = win_friendly_path(File.join(Dir.tmpdir(), "#{setup_zip}.log"))
-service_install_batch_file = win_friendly_path(File.join(swarm_install_path, "bat", "installService.bat"))
+service_install_batch_file = win_friendly_path(File.join(service_install_path, "bat", "installService.bat"))
 
 cookbook_file setup_zip_temp_path do
 	source setup_zip
@@ -39,7 +39,7 @@ cookbook_file setup_zip_temp_path do
 	not_if { service_is_installed }
 end
 
-windows_zipfile swarm_install_path do
+windows_zipfile service_install_path do
 	source setup_zip_temp_path
 	action :unzip
 	not_if { service_is_installed }
@@ -49,21 +49,25 @@ remote_file swarm_client_path do
 	source node['jenkinsswarmservice']['url']
 end
 
-template "swarm_config_file" do
-	:auto_discovery_addressnode => node['jenkinsswarmservice']['parameters']['auto_discovery_address']
-	:disable_ssl_verification => node['jenkinsswarmservice']['parameters']['disable_ssl_verification']
-	:executors => node['jenkinsswarmservice']['parameters']['executors']
-	:fs_root => node['jenkinsswarmservice']['parameters']['fs_root']
-	:labels => node['jenkinsswarmservice']['parameters']['labels']
-	:master => node['jenkinsswarmservice']['parameters']['master']
-	:mode => node['jenkinsswarmservice']['parameters']['mode']
-	:name => node['jenkinsswarmservice']['parameters']['name']
-	:password => node['jenkinsswarmservice']['parameters']['password']
-	:username => node['jenkinsswarmservice']['parameters']['username']
-	:install_path => node['jenkinsswarmservice']['install_path']
-	:jar_file => swarm_client_path
-	:service_user => node['jenkinsswarmservice']['service_user']
-	:service_user_password => node['jenkinsswarmservice']['service_user_password']
+template service_config_file do
+	source "conf_template.conf.erb"
+	variables({
+		:auto_discovery_addressnode => node['jenkinsswarmservice']['parameters']['auto_discovery_address'],
+		:description => node['jenkinsswarmservice']['parameters']['description'],
+		:disable_ssl_verification => node['jenkinsswarmservice']['parameters']['disable_ssl_verification'],
+		:executors => node['jenkinsswarmservice']['parameters']['executors'],
+		:fs_root => node['jenkinsswarmservice']['parameters']['fs_root'],
+		:labels => node['jenkinsswarmservice']['parameters']['labels'],
+		:master => node['jenkinsswarmservice']['parameters']['master'],
+		:mode => node['jenkinsswarmservice']['parameters']['mode'],
+		:name => node['jenkinsswarmservice']['parameters']['name'],
+		:password => node['jenkinsswarmservice']['parameters']['password'],
+		:username => node['jenkinsswarmservice']['parameters']['username'],
+		:install_path => node['jenkinsswarmservice']['install_path'],
+		:jar_file => swarm_client_path,
+		:service_user => node['jenkinsswarmservice']['service_user'],
+		:service_user_password => node['jenkinsswarmservice']['service_user_password']
+	})
 end
 
 execute "Install service" do 
